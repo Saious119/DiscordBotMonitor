10331c507a82e6e72b31f156ab9e573bccb4a67e
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-02-14 22:02:39 -0500

    blazor on fortrash

---
+++ DBM/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+app.UseAuthentication();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapRazorPages();
+
+app.Run();
