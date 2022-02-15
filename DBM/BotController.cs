ed15545929195623fe3ab6992b3832cae7d89952
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-02-15 13:07:36 -0500

    adding Dick JOhnson

---
+++ DBM/BotController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace DBM
+{
+    public class BotController
+    {
+        public string BotStatus = "Offline :'(";
+        Process CurrentProc;
+        string BotName;
+        public BotController(string Botname)  { BotName = Botname; } 
+
+        public void StartBot(string startscript)
+        {
+            if (BotStatus != "Online")
+            {
+                try
+                {
+
+                    CurrentProc = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = startscript,
+                            Arguments = "",
+                            UseShellExecute = false,
+                            WorkingDirectory = "/home/andym/DiscordBotMonitor/DBM",
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = false
+                        }
+                    };
+                    CurrentProc.Start();
+                    BotStatus = "Online";
+                }
+                catch (Exception e)
+                {
+                    BotStatus = "Offline :'(";
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+        public void StopBot()
+        {
+            if (BotStatus == "Online")
+            {
+                CurrentProc.Kill(true);
+                CurrentProc.Dispose();
+                BotStatus = "Offline";
+            }
+        }
+    }
+}
---
+++ DBM/StartDickJohnson.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true 
+do
+	dotnet ../../../Discord-Bots/DickJohnson
+done
